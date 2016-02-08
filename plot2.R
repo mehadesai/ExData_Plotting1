57302e9efc87ea0dd371b3aa679122ac15237513
Meha Desai <meha@urbanladder.com>
2016-02-08 14:36:03 +0530

    code to plor global active power by week

---
+++ plot2.R
@@ -0,0 +1,14 @@
+file_name <- 'household_power_consumption.txt'
+
+pwr_cnsmptn <- read.table(file_name, header = TRUE, sep = ';', na.strings='?')
+dates <- c('1/2/2007', '2/2/2007')
+subset_pwr_cnsmptn <- pwr_cnsmptn[pwr_cnsmptn$Date %in% dates, ]
+
+date_time_info <- paste( subset_pwr_cnsmptn$Date, subset_pwr_cnsmptn$Time )
+subset_pwr_cnsmptn$DateTime <- strptime( date_time_info, format = '%d/%m/%Y %H:%M:%S' )
+
+png(filename = 'plot2.png', width = 480, height = 480)
+plot(subset_pwr_cnsmptn$DateTime, subset_pwr_cnsmptn$Global_active_power,
+     type='l',
+     xlab='', ylab='Global Active Power (kilowatts)')
+dev.off()
